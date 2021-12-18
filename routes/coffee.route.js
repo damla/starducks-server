c97023deed44e61470cdb602497679eb89ab8c69
Damla Koksal <me@damlakoksal.com>
2021-12-18 13:24:31 +0300

    [#2] style: add controller folder

--- routes/coffee.route.js
+++ routes/coffee.route.js
@@ -1,16 +1,16 @@
 import express from 'express'
+import coffeeController from '../controllers/coffee.controller.js'
+
 const router = express.Router()
-import { v4 as uuidv4 } from 'uuid'
 
-// uuidv4()
-// uuid.parse(str)
+router.get('/', (req, res) => {
+  coffeeController.getCoffee(req, res)
+})
+// router.post('/', (req, res) => {})
 
-router.get('/', (req, res) => {})
-router.post('/', (req, res) => {})
-
-// coffee/{id}
-router.get('/:id', (req, res) => {})
-router.delete('/:id', (req, res) => {})
-router.put('/:id', (req, res) => {})
+// // coffee/{id}
+// router.get('/:id', (req, res) => {})
+// router.delete('/:id', (req, res) => {})
+// router.put('/:id', (req, res) => {})
 
 export default router
